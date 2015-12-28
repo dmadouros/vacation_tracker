b42b496f178f178029d1afa8b68264dedf9dd246
Kim D Barnes <kimdbarnes@gmail.com>
2015-12-28 16:15:26 -0700

    Introduce PtoRequestCollection for handling calculations of related pto_requests
    [#110703938]

---
+++ app/presenters/pto_request_collection_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe PtoRequestCollection do
+  let(:pto_requests) do
+    user = build(:user)
+    [
+        build(:pto_request, user: user, hours: 8),
+        build(:pto_request, user: user, hours: 10, floating_holiday: true),
+        build(:pto_request, user: user, hours: 3),
+    ]
+  end
+
+  describe '#pto_requests' do
+    it 'should return presented pto_requests' do
+      expect(described_class.new(pto_requests).pto_requests.first).to be_an_instance_of PtoRequestPresenter
+    end
+  end
+
+  describe '#empty?' do
+    context 'when no pto_requests' do
+      it 'should be true' do
+        expect(described_class.new([]).empty?).to eq true
+      end
+    end
+
+    context 'when some pto_requests' do
+      it 'should be false' do
+        expect(described_class.new(pto_requests).empty?).to eq false
+      end
+    end
+  end
+
+  describe '#any_floating_holiday_hours?' do
+    context 'when some floating holiday hours' do
+      it 'should be true' do
+        expect(described_class.new([build(:pto_request, hours: 8, floating_holiday: true)]).any_floating_holiday_hours?).to eq true
+      end
+    end
+
+    context 'when no floating holiday hours' do
+      it 'should be false' do
+        expect(described_class.new([build(:pto_request, hours: 8, floating_holiday: false)]).any_floating_holiday_hours?).to eq false
+      end
+    end
+  end
+
+  describe '#total_pto_hours' do
+    it 'should return the total' do
+      expect(described_class.new(pto_requests).total_pto_hours).to eq 11
+    end
+  end
+
+  describe '#total_floating_holiday_hours' do
+    it 'should return the total' do
+      expect(described_class.new(pto_requests).total_floating_holiday_hours).to eq 10
+    end
+  end
+end
--- app/services/notification_mailer.rb
+++ app/services/notification_mailer.rb
@@ -1,8 +1,6 @@
 class NotificationMailer < ActionMailer::Base
   def monthly_vacation_status(user, pto_requests, report_date)
-    @pto_requests = presented_pto_requests(pto_requests)
-    @total_hours = total_hours(pto_requests)
-    @floating_holiday_hours = floating_holiday_hours(pto_requests)
+    @pto_request_collection = PtoRequestCollection.new(pto_requests)
     @report_date = report_date
     @user = user
 
@@ -13,23 +11,5 @@ class NotificationMailer < ActionMailer::Base
 
   def subject_line_for(report_date)
     "PTO Report for #{report_date.strftime('%B %Y')}"
-  end
-
-  def total_hours(pto_requests)
-    deductible_pto_requests(pto_requests) || 0
-  end
-
-  def floating_holiday_hours(pto_requests)
-    pto_requests.select(&:floating_holiday?).map(&:hours).reduce(:+)
-  end
-
-  def deductible_pto_requests(pto_requests)
-    pto_requests.reject(&:floating_holiday?).map(&:hours).reduce(:+)
-  end
-
-  def presented_pto_requests(pto_requests)
-    pto_requests.map do |pto_request|
-      PtoRequestPresenter.new(pto_request)
-    end
   end
 9end
